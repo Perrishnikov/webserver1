f084ba662f13b6713552916d3db1ea697625a869
Perrishnikov <perry@Perrys-MacBook-Pro.local>
2017-01-10 00:05:39 -0600

    hello again

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    console.log('Hello World!');
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
